9ce5250169dfc0502aa26c5b1ea41838eee87f1e
pjc1991 <whdekf@naver.com>
2021-04-16 17:16:02 +0900

    admin search added.

--- pybo/views.py
+++ pybo/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 
 # Create your views here.
 
 def index(request):
+    """
+    pybo 목록 출력
+    """
+
+    question_list = Question.objects.order_by('-created_date')
+    context = { 'question_list' : question_list}
     return HttpResponse("Hello, pybo!")
