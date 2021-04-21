505ca99abf854348f75c6dba1a203f84c5554dda
pjc1991 <whdekf@naver.com>
2021-04-21 23:12:48 +0900

    question board created

--- pybo/views.py
+++ pybo/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-from .models import Question
+from pybo.models import Question
 
 # Create your views here.
 
@@ -11,4 +11,13 @@ def index(request):
 
     question_list = Question.objects.order_by('-created_date')
     context = { 'question_list' : question_list}
-    return HttpResponse("Hello, pybo!")
+    return render(request, 'pybo/question_list.html', context)
+
+def detail(request, question_id):
+    """
+    pybo 목록 상세 출력
+    """
+
+    q = get_object_or_404(Question, pk=question_id)
+    context = { 'question': q }
+    return render(request, 'pybo/question_detail.html', context)
