3d54d38a0d20371a5f8925ac8360fe2768367766
pjc1991 <pjc1991@gmail.com>
2021-06-29 21:37:02 +0900

    add answer function

--- pybo/models.py
+++ pybo/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Question(models.Model):
     subject = models.CharField(max_length=200)
     content = models.TextField()
-    created_date = models.DateTimeField()
+    create_date = models.DateTimeField()
 
     def __str__(self):
         return self.subject
@@ -13,7 +13,7 @@ class Question(models.Model):
 class Answer(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     content = models.TextField()
-    created_date = models.DateTimeField()
+    create_date = models.DateTimeField()
     
     def __str__(self):
         return self.content
--- pybo/views.py
+++ pybo/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect, render, get_object_or_404
 from django.http import HttpResponse
 from pybo.models import Question
+from django.utils import timezone
 
 # Create your views here.
 
@@ -9,7 +10,7 @@ def index(request):
     pybo 목록 출력
     """
 
-    question_list = Question.objects.order_by('-created_date')
+    question_list = Question.objects.order_by('-create_date')
     context = { 'question_list' : question_list}
     return render(request, 'pybo/question_list.html', context)
 
@@ -19,5 +20,15 @@ def detail(request, question_id):
     """
 
     q = get_object_or_404(Question, pk=question_id)
-    context = { 'question': q }
+    answer_list = q.answer_set.all()
+    context = { 'question': q , 'answer_list' : answer_list}
     return render(request, 'pybo/question_detail.html', context)
+
+def answer_create(request, question_id):
+    """
+    pybo 답변 등록
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
+
+    return redirect('pybo:detail', question_id=question_id)
