9e396f4e577ac6d2af25fc35e128803f0c3a6a4b
pjc1991 <whdekf@gmail.com>
2023-05-22 16:22:19 +0900

    style : add blank lines

--- pybo/forms.py
+++ pybo/forms.py
@@ -1,7 +1,9 @@
 from django import forms
 from pybo.models import Question
 
+
 class QuestionForm(forms.ModelForm):
+
     class Meta:
         model = Question
         fields = ['subject', 'content']
