bfee435adfa72d3bfde6f3144d0544c380d1ec87
pjc1991 <whdekf@gmail.com>
2023-05-23 06:45:42 +0900

    feat : add login/logout

--- config/settings.py
+++ config/settings.py
@@ -125,3 +125,6 @@ STATICFILES_DIRS = [
 ]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
--- config/urls.py
+++ config/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+import pybo
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('pybo/', include('pybo.urls')),
     path('common/', include('common.urls')),
+    path('', pybo.views.index, name='index'),
 ]
