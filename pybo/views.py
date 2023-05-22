5ad52850eaafeba0108076247a568d55ab9fec95
pjc1991 <whdekf@gmail.com>
2023-05-23 05:50:05 +0900

    feat : add pagination

--- pybo/views.py
+++ pybo/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from pybo.models import Question, Board
 from django.utils import timezone
 from .forms import QuestionForm
+from django.core.paginator import Paginator
 
 # Create your views here.
 
@@ -11,9 +12,12 @@ def index(request):
     """
     pybo 목록 출력
     """
-
+    page = request.GET.get('page', '1')  # 페이지
     question_list = Question.objects.order_by('-create_date')
-    context = {'question_list': question_list}
+    paginator = Paginator(question_list, 10)  # 페이지당 10개씩 보여주기
+    page_obj = paginator.get_page(page)
+    page_obj.elided_page_range = paginator.get_elided_page_range(page)
+    context = {'question_list': page_obj}
     return render(request, 'pybo/question_list.html', context)
 
 
